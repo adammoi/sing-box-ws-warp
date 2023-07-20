9766ece8194c4777d4d108d8fc7f215cf309ecf1
adammoi <adamspx17@gmail.com>
2023-07-21 01:49:05 +0700

    change to pem file

--- cert.sh
+++ cert.sh
@@ -5,19 +5,22 @@ echo " [INFO] Cert Zone "
 sleep 1
 
 domain=$(cat /root/vps/domain.txt)
+mkdir -p /root/cert/$domain
 cek=$(lsof -i:80 | awk 'NR==2 {print $1}')
 systemctl stop nginx
 
 if [[ ! -z "$cek" ]]; then
     sleep 1
     echo " [ WARNING ] Detected port 80 used by $cek "
+    sleep 1
     echo " [ INFO ] Processing to stop $cek "
+    sleep 1
     systemctl stop $cek
 else
     echo " [ INFO ] Port 80 is not in use "
 fi
 
-echo " [ INFO ] Starting renew cert... "
+echo " [ INFO ] Starting to renew cert... "
 sleep 1 
 
 # Cek apakah file /root/.acme.sh/acme.sh tidak ada
@@ -31,7 +34,7 @@ if [ ! -f "/root/.acme.sh/acme.sh" ]; then
     ./acme.sh --upgrade --auto-upgrade
     ./acme.sh --set-default-ca --server letsencrypt
     ./acme.sh --issue -d $domain --standalone -k ec-256
-    ./acme.sh --installcert -d $domain --fullchainpath /root/cert/sing-box.crt --keypath /root/cert/sing-box.key --ecc
+    ./acme.sh --installcert -d $domain --fullchainpath /root/cert/$domain/fullchain.pem --keypath /root/cert/$domain/privkey.pem --ecc
 
     echo -e " [ INFO ] Renew cert done... " 
     sleep 1
@@ -53,7 +56,7 @@ else
     ./acme.sh --upgrade --auto-upgrade
     ./acme.sh --set-default-ca --server letsencrypt
     ./acme.sh --issue -d $domain --standalone -k ec-256
-    ./acme.sh --installcert -d $domain --fullchainpath /root/cert/sing-box.crt --keypath /root/cert/sing-box.key --ecc
+    ./acme.sh --installcert -d $domain --fullchainpath /root/cert/$domain/fullchain.pem --keypath /root/cert/$domain/privkey.pem --ecc
 
     echo -e " [ INFO ] Renew cert done... " 
     sleep 1
