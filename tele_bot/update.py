26d19598aad6e720ff04c16c89ba547df7571d64
adammoi <adamspx17@gmail.com>
2023-07-21 23:53:50 +0700

    delete

--- tele_bot/update.py
+++
@@ -1,5 +0,0 @@
-import os
-print('--------> Downloading tele_bot.py\n\n')
-os.system('curl -Lo /root/tele_bot/tele_bot.py https://raw.githubusercontent.com/adammoi/sing-box-ws-warp/main/tele_bot.py')
-os.system('systemctl restart tele_bot.service')
-print('Done, enjoy!')
